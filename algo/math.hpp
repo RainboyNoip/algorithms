79362dca54f36d3cd4859fb900d1dd26a9caad13
rainboy <rainboylvx@qq.com>
2021-08-31 16:27:06 +0800

    update

--- algo/math.hpp
+++ algo/math.hpp
@@ -1,1 +1,31 @@
 //所有数学相关的算法
+#pragma 
+#include <algo/base.hpp>
+
+
+namespace __MY_SELF__NAMESPACE__ {
+
+template<typename T>
+inline T gcd(T a,T b){
+    while ( b ) {
+        T t = a % b;
+        a = b,b = t;
+    }
+    return a;
+}
+
+template<typename T>
+inline T lcm(T a,T b){
+    return a/gcd(a,b) * b;
+}
+
+template<typename T>
+T exgcd(T a,T b,T& x, T& y){
+    //TODO
+}
+
+
+
+
+} //namespace __MY_SELF__NAMESPACE__
+
