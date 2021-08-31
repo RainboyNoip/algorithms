1ffcb89af557f36d4635bcadf2ae7d46d2d73816
rainboy <rainboylvx@qq.com>
2021-08-31 10:47:55 +0800

    update

---
+++ include/linked_forward_start.hpp
@@ -0,0 +1,60 @@
+// https://zh.wikipedia.org/wiki/%E9%93%BE%E5%BC%8F%E5%89%8D%E5%90%91%E6%98%9F
+//[链式前向星及其简单应用 | Malash's Blog](https://malash.me/200910/linked-forward-star/)
+
+#pragma once
+#include <cstddef>
+#include <iostream>
+
+
+/**
+* 说明:不可以有编号为0的点
+* 使用: TODO
+*/
+template<size_t _edge_cnt=1000005,size_t _vex_cnt=200005>
+struct linkedForwardStar {
+    struct Itertor;
+    struct edge{ 
+        int v,w; edge * nxt;
+        friend std::ostream& operator<<(std::ostream& __out,const edge & __e){
+            __out << "v: " << __e.v;
+            __out << " w: " << __e.w;
+            return __out;
+        }
+    };
+    edge e[_edge_cnt];
+    edge *head[_vex_cnt]={nullptr}; size_t edge_cnt{0};
+
+    struct Itertor {
+        edge * _p_edge;
+        //Itertor() :_p_edge{nullptr} {}
+        Itertor(edge * __pe) :_p_edge{__pe} {}
+        void operator++(){ _p_edge = _p_edge->nxt;}
+        edge& operator*(){ return *_p_edge; }
+        //void operator++(int){ ++*this; }
+        //friend bool operator==(const Itertor &__x,const Itertor &__y){
+            //return __x._p_edge == __y._p_edge;
+        //}
+        friend bool operator==(const Itertor &__x,decltype(nullptr)){
+            return __x._p_edge == nullptr; 
+        }
+
+        Itertor& begin(){ return *this;}
+        //auto end(){return Itertor{nullptr};}
+
+        //c++17 requires
+        auto end(){return nullptr;}
+    };
+    void add(int u,int v,int w=0){
+        e[edge_cnt] = {v,w,head[u]};
+        head[u] = &e[edge_cnt++];
+    }
+    void add2(int u,int v,int w=0){
+        add(u,v,w);
+        add(v,u,w);
+    }
+
+    //edge& 
+    Itertor operator[](int __vex){
+        return Itertor{head[__vex]};
+    }
+};
--- readme.md
+++ readme.md
@@ -1,14 +1,51 @@
 ## 目的
 
-受[xtaci/algorithms: Algorithms & Data structures in C++.](https://github.com/xtaci/algorithms) 启发，把写题目用到的算法与数据结构写出来，快速写题目。
+受[xtaci/algorithms: Algorithms & Data structures in C++.](https://github.com/xtaci/algorithms) 启发，把写题目用到的算法与数据结构写出来，用于：
 
-#### 约定 ( Convention ):
+1. 快速写题目。
+2. 个人算法总结
+3. 算法学习
 
-   1.  一个算法用一个`.hpp`文件表示放到include下. ( one .header file per algorithm. )
-   2.  算法演示的demo程序放到src下.  ( one demo per algorithm.  )
-   3.  TAB = 4 space.  set ts=4 in vim
-   4.  Graph的输出格式为 [Graphviz](http://www.graphviz.org/) Dot格式.  (the output format of the graph is in dot of graphviz.)
+## 约定 ( Convention ):
 
+   1. 一个算法用一个`.hpp`文件表示放到include下. ( one .header file per algorithm. )
+   2. 算法演示的demo程序放到src下.  ( one demo per algorithm.  )
+   3. TAB = 4 space.  set ts=4 in vim
+   4. Graph的输出格式为 [Graphviz](http://www.graphviz.org/) Dot格式.  (the output format of the graph is in dot of graphviz.)
+   5. 易用 easy to use
+   6. 代码即文档： 只需要看代码(或代码注释)就可以学会如何使用
+   7. 头文件提供源码：只用包含头文件即可使用
+
+## 安装与更新
+
+```bash
+# 下载本仓库到
+g++ clone https://github.com/RainboyNoip/algorithms ~/.algorithms
+vim ~/.zshrc # 这个由你使用的终端来决定
+
+# 手动添加如下
+# 修改默认g++ 查询include的路径
+export CPLUS_INCLUDE_PATH=$HOME/.algorithms:$CPLUS_INCLUDE_PATH
+## 为了使用algorithms 下的工具
+export PATH=$PATH:$HOME/.algorithms/bin
+```
+
+其它
+
+```bash
+# 安装graphviz
+sudo apt install graphviz
+```
+
+bin目录下的工具有 TODO
 ```plaintext
-./bin 包含python，cpp，常用工具的tools
+bin
+├── b               编译当前目录下字典序最小的cpp文件，并自动` < in`,具体查看`b --help`
+├── dot2png.py
+├── dp.py
+├── in2dot
+├── lsoitools
+├── oi-show.py
+├── randint.py
+└── r_test.js
 ```
