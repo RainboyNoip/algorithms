dd06757087e83ef12471c83911427533544fc389
rainboy <rainboylvx@qq.com>
2023-07-27 06:33:45 +0800

    add inf and INF

--- include/base/macro.hpp
+++ include/base/macro.hpp
@@ -33,6 +33,7 @@ using std::literals::string_view_literals::operator""sv;
 #define __M_concat_sv(a) __M_concat_I(a,sv)
 
 
+//将 "2e5" 这个的字符串转数字
 constexpr int _sexp_to_number(std::string_view _sv){
     int num = 0,e = 0,i=0;
     while( i < _sv.length() && _sv[i] !='e')
@@ -41,17 +42,77 @@ constexpr int _sexp_to_number(std::string_view _sv){
     while( i < _sv.length() )
         e = e*10 + _sv[i]-'0',++i;
     while(e--) num = num*10;
-    return num+5;
+    return num;
+}
+constexpr int add_five(int number) {
+    return number + 5;
 }
 
+long long operator ""_Number(const char * str,std::size_t N){
+    return _sexp_to_number(std::string_view(str,N));
+}
+
+
+//得到数值的最值
+struct __INF {
+    template<typename T>
+    constexpr operator T() const {
+        return std::numeric_limits<T>::max();
+    }
+
+    template<typename T>
+    constexpr bool operator==(const T oth) const
+    {
+        return T(*this) == oth;
+    }
+
+};
+
+struct __NINF {
+    template<typename T>
+    constexpr operator T() const {
+        return std::numeric_limits<T>::lowest();
+    }
+
+    template<typename T>
+    constexpr bool operator==(const T oth) const
+    {
+        return T(*this) == oth;
+    }
+};
+
+constexpr auto NINF = __NINF();
+constexpr auto INF  = __INF();
+
+//得到数值的相对最值 0x3f
+struct __inf {
+    constexpr operator unsigned long long() const { return 0x3f3f3f3f3f3f3f3f; }
+    constexpr operator long long() const  { return 0x3f3f3f3f3f3f3f3f; }
+    constexpr operator unsigned int() const { return 0x3f3f3f3f; }
+    constexpr operator int() const { return 0x3f3f3f3f; }
+    constexpr operator char() const { return 0x3f; }
+    constexpr operator unsigned char() const { return 0x3f; }
+
+    template<typename T>
+    constexpr bool operator==(const T oth) const
+    {
+        return T(*this) == oth;
+    }
+};
+
+constexpr auto inf = __inf();
+
+
+
+
 #ifdef __maxn__
-const int maxn = _sexp_to_number(__M_concat_sv(__maxn__));
+const int maxn = add_five(_sexp_to_number(__M_concat_sv(__maxn__)));
 #elif not defined (maxn)
 const int maxn = 1000007;
 #endif
 
 #ifdef __maxe__
-const int maxe = _sexp_to_number(__M_concat_sv(__maxe__));
+const int maxe = add_five(_sexp_to_number(__M_concat_sv(__maxe__)));
 #elif not defined (maxe)
 const int maxe = 1000007;
 #endif
