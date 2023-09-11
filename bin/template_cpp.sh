de31d938b7d3207872b83d0f9c6b14f2e4f045ef
rainboy <rainboylvx@qq.com>
2023-09-11 10:59:44 +0800

    add copy template script

--- bin/template_cpp.sh
+++ bin/template_cpp.sh
@@ -1,13 +1,32 @@
-#!/bin/bash
+#bin!/bin/bash
 #把 代码模板 生成成当前文件
 filename="${1:-1.cpp}"
 if [[ ! "$filename" =~ \.cpp$ ]]; then
   filename="${filename}.cpp"
 fi
 # 获取当前脚本所在的目录
-script_dir=$(dirname "$0")/..
+script_dir=$(dirname "$0")/../template
 
 # 复制 a.cpp 到当前运行目录下
-cp "$script_dir/template.cpp" ./$filename
+choose=$(find $script_dir -maxdepth 2 -type f -name "*" -printf "%P\n" | fzf \
+        --height=~20% --tac --layout=reverse --border=top \
+        --border-label="choose input" --border-label-pos=4 \
+        --select-1 --ansi
+    )
 
-echo "复制成功！当前目录下的文件 "$filename
+if [ -z "$choose" ];then
+    echo "没有选择任何模板"
+    exit 0
+fi
+
+if [ -f $filename ];then 
+    echo "当前目录存在文件 $filename"
+    read -p "是否替换(y/N):" choice
+    if [[ "$choice" == "N" || "$choice" == "n" || "$choice" == "" ]];then
+        exit 0
+    fi
+fi
+
+cp "$script_dir/$choose" $filename
+
+echo "复制成功！当前目录下的文件: "$filename
--- template/template.cpp
+++ template/template.cpp
@@ -1,7 +1,3 @@
-/*-----------------
-* author: Rainboy  | Mail: rainboylvx@qq.com | Write_time: {time}
-* if you want to compile this code , please visit : https://github.com/RainboyNoip/algorithms
-*----------------*/
 #define __maxn__ 1e6
 #define __maxe__ 2e6
 #include "base.hpp"
@@ -36,16 +32,15 @@ int n,m,root;
 
 
 //数据读取 
-inline void init() {{
+inline void init() {
     in(n,m);
-    {ipos}
-}}
+}
 
 
-int main(){{
+int main(){
 #ifdef DEBUG
     Clock t("main time");
 #endif
     init();
     return 0;
-}}
+}
